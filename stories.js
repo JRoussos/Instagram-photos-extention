478fd10734d5bcde8fbe8df28d7e42bb7d2cb8a7
John Roussos <giannhs632@gmail.com>
2020-12-09 21:24:12 +0200

    fixing my mistakes

--- stories.js
+++ stories.js
@@ -1,6 +1,6 @@
     source = document.getElementsByClassName("qbCDp");
     if(source.length > 0){
-        window.open(source[0].getElementsByTagName("img")[0].getAttribute("src"));
+        window.open(source[0].getElementsByTagName("img")[0].getAttribute("srcset").split(',')[0]);
         video = source[0].getElementsByTagName("video");
         if(video.length > 0)
             window.open(video[0].getElementsByTagName("source")[0].getAttribute("src"));
